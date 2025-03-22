5c860a76ddb73f51bae22014b31b8fa49b7595bb
shiki-k-1225 <4949gtmw@gmail.com>
2025-03-22 18:07:20 +0900

    before_actionでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,28 +20,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item は before_action で設定済み
   end
 
   def edit
-    @item = Item.find(params[:id])
-
-    # 売却済み商品の場合はトップページにリダイレクト（購入機能語実装の為コメントアウト処理）
-  # if @item.order.present?
-  #   redirect_to root_path
-  # end
+    # 売却済み商品の場合はトップページにリダイレクト（購入機能後実装の為コメントアウト処理）
+    # if @item.order.present?
+    #   redirect_to root_path
+    # end
 
     redirect_to root_path unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
-
-    # 編集時に画像が未変更の場合、既存の画像を保持
-    if params[:item][:image].nil?
-      params[:item][:image] = @item.image.blob if @item.image.attached?
-    end
-
+    # @item は before_action で設定済み
     # 商品情報の更新処理
     if @item.update(item_params)
       redirect_to item_path(@item), notice: '商品情報が更新されました。'
@@ -50,6 +43,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
