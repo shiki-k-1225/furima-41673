7aa2901839c86faf3dcdbda3384c7a9e86bd22b2
shiki-k-1225 <4949gtmw@gmail.com>
2025-03-24 14:11:40 +0900

    必要な情報を適切に入力して「購入」ボタンを押すと、商品の購入ができること。

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,45 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_if_invalid, only: [:index, :create]
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      pay_item # PAY.JP決済
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_address_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_if_invalid
+    if @item.purchase.present? || current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,24 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id, :token
+
+  # バリデーション
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁-4桁」の形式で入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" } # 初期値「--」を除外
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の半角数字で入力してください" }
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
+
+  # 保存処理
+  def save
+    # 購入情報を保存
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    # 配送先情報を保存
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
